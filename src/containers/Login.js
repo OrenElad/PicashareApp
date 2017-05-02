e1e9c61e61e3ae667b2751e21ccdc6505a9fd966
oren <elador1@gmail.com>
2017-05-02 20:51:29 +0300

    added login assets

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -56,14 +56,33 @@ class Login extends Component {
 
   render() {
     return (
-      <View style={{flex:1,top: 30, flexDirection:'column',justifyContent:'flex-start',alignItems:'center'}}>
+      <View style={{flex:1,top: 30, flexDirection:'column',justifyContent:'space-around',alignItems:'center'}}>
         <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
           <Image style={styles.image} source={{uri:"https://res.cloudinary.com/marvel-content/image/fetch/s--zoWuAqCa--/f_auto,fl_lossy,q_100,t_app_icon_120/https://marvelapp-live.storage.googleapis.com/serve/2017/4/f635c10b995b4d2a9ee7cf7514be79d0.jpg"}}></Image>
           <Text style={{fontSize:28,color: '#a3c034', fontWeight: '700'}}>Picashare</Text>
         </View>
-        <Text style={{top: 20, fontSize:16, color: '#63a9d3', textAlign: 'center', fontWeight:'500', marginLeft: 10, marginRight: 10}}>join over 500,000 picashares and Use free amazing & powerful platform to make money today</Text>
-        <Text style={{top: 100,fontSize:20,color: '#63a9d3'}}>Log-in By</Text>
-
+        <Text style={{fontSize:16, color: '#63a9d3', textAlign: 'center', fontWeight:'500', marginLeft: 10, marginRight: 10}}>join over 500,000 picashares and Use free amazing & powerful platform to make money today</Text>
+        <Text style={{fontSize:20,color: '#63a9d3'}}>Log-in By</Text>
+        <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+        <TouchableOpacity onPress={this._}>
+          <Image
+            style={styles.button}
+            source={require('../assets/facebook-login.png')}
+          />
+        </TouchableOpacity>
+          <TouchableOpacity onPress={this._}>
+          <Image
+            style={styles.button}
+            source={require('../assets/google-plus-login.png')}
+          />
+        </TouchableOpacity>
+          <TouchableOpacity onPress={this._}>
+          <Image
+            style={styles.button}
+            source={require('../assets/twitter-login.png')}
+          />
+        </TouchableOpacity>
+        </View>
         {/*<View><LoginButton
           publishPermissions={["public_profile"]}
           onLoginFinished={
@@ -94,6 +113,10 @@ var styles = StyleSheet.create({
     height: 75,
     marginBottom:30,
     left: 10
+  },
+  button: {
+    width: 50,
+    height: 50
   }
 });
 
